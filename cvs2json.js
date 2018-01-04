b289384376776e70429f871c332028103bc6bde5
Kasper Sort <kasper.sort@deic.dk>
2018-01-04 13:08:04 +0100

    Add files via upload

--- cvs2json.js
+++ cvs2json.js
@@ -1,29 +1,26 @@
-const request = require('request')
 const path    = require('path')
 const csv     = require('csvtojson')
 const fs      = require('fs')
 
-const outFile = 'customer-data.json'
-const csvFile = 'customer-data.csv'
+const outFile = path.join(__dirname, 'customer-data.json')
+const csvFile = path.join(__dirname, 'customer-data.csv')
 
 const csv2JSON = (inData) => { 
 	let json = []
 
 	csv()
-	.fromStream(request.get(inData))
+	.fromFile(csvFile)
 
 	.on('json',(jsonObj) => {
 	    json.push(jsonObj)
 	})
 
 	.on('done',(error) => {
-		fs.writeFile(path.join(__dirname, outFile), JSON.stringify(json, null, 2), 'utf8', (error) => {
+		fs.writeFile(outFile, JSON.stringify(json, null, 2), 'utf8', (error) => {
 			if (error)  throw error
 			console.log('File saved')
 		})
 	})
 }
 
-csv2JSON(process.argv[2])
-
-//URL: 'https://prod-edxapp.edx-cdn.org/assets/courseware/v1/07d100219da1a726dad5eddb090fa215/asset-v1:Microsoft+DEV283x+2T2017+type@asset+block/customer-data.csv'
+csv2JSON()
